816aa738ea2995ac0720a413be202be62a40c2a2
AkshDesai04 <dev.akshdesai@gmail.com>
2022-07-15 11:45:32 -0700

    Added an AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text("SUP!"),
+void main() => runApp (
+	MaterialApp (
+  		home: Scaffold (
+			appBar: AppBar(
+				title: Text("Sup!")
+			),
+		),
 ));
