354b51d116093e2b6877fbef5ad591826db20d5f
AkshDesai04 <dev.akshdesai@gmail.com>
2022-07-16 04:22:13 -0700

    Commented out the text and added image using NetworkImage

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Home(),
-    ));
+  )
+);
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          "Sup Dude! \nThis is a Text",
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2,
-            color: Colors.grey[600],
-            backgroundColor: Colors.red[300],
-            fontFamily: 'PermanentMarker',
-          ),
-        ),
+        // child: Text(
+        //   "Sup Dude! \nThis is a Text\nBellow is an image for u from the internet!",
+        //   style: TextStyle(
+        //     fontSize: 30,
+        //     fontWeight: FontWeight.bold,
+        //     letterSpacing: 2,
+        //     color: Colors.grey[600],
+        //     backgroundColor: Colors.red[300],
+        //     fontFamily: 'PermanentMarker',
+        //   ),
+        // ),
+        child: Image(
+          image: NetworkImage('https://cdn.pixabay.com/photo/2020/06/08/03/13/plant-5272770_960_720.jpg'),
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
