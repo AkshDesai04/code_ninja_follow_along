1bce0bad362ea985fc3ffbf240c48c2b12487733
AkshDesai04 <dev.akshdesai@gmail.com>
2022-07-16 01:11:28 -0700

    Removed AppBar coz it sucks

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp (
-	MaterialApp (
-  		home: Scaffold (
-			appBar: AppBar(
-				title: Text("Sup!"),
-				centerTitle: true,
-				backgroundColor: Colors.red,
-			),
-			body: Center(
-				child: Text(
-					"Sup Dude! \nThis is a Text",
-					style: TextStyle(
-						fontSize: 30,
-						fontWeight: FontWeight.bold,
-						letterSpacing: 2,
-						color: Colors.grey[600],
-						backgroundColor: Colors.red[300],
-						fontFamily: 'PermanentMarker',
-					),
-				),
-			),
-			floatingActionButton: FloatingActionButton(
-				onPressed: () {  },
-				child: Text("Click"),
-				backgroundColor: Colors.yellow[900],
-			),
-		),
-));
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "Sup Dude! \nThis is a Text",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.grey[600],
+              backgroundColor: Colors.red[300],
+              fontFamily: 'PermanentMarker',
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("Click"),
+          backgroundColor: Colors.yellow[900],
+        ),
+      ),
+    ));
+  
