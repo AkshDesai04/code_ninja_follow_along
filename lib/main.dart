9c01a8a3cad8c958f506bbf2860e46edbad3c21c
AkshDesai04 <dev.akshdesai@gmail.com>
2022-07-16 04:06:30 -0700

    Made it dry by using a statless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            "Sup Dude! \nThis is a Text",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
-              color: Colors.grey[600],
-              backgroundColor: Colors.red[300],
-              fontFamily: 'PermanentMarker',
-            ),
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Sup Dude! \nThis is a Text",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            color: Colors.grey[600],
+            backgroundColor: Colors.red[300],
+            fontFamily: 'PermanentMarker',
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text("Click"),
-          backgroundColor: Colors.yellow[900],
-        ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Click"),
+        backgroundColor: Colors.yellow[900],
+      ),
+    );
+  }
+}
